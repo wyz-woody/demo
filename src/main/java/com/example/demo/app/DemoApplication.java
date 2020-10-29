c280b70230f4dd5d1fe357f66267b3afed0037bd
za-wuyongzhi <yongzhi.wu@zatech.com>
2020-10-29 18:07:07 +0800

    init

---
+++ src/main/java/com/example/demo/app/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScans;
+
+@SpringBootApplication(scanBasePackages = {"com.example.demo"})
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ */
+@RestController
+@RequestMapping("demo/")
+public class DemoController {
+    @RequestMapping("home")
+    String home(HttpServletRequest request) {
+        String userName = "Jef";
+        int count = 100;
+        return "欢迎您" + userName + "，这是您的第 " + count + " 次登录";
+    }
+
+}
