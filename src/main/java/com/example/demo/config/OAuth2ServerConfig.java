32c343fe260ead527e73b677ce0c4f7c9d60d2de
za-wuyongzhi <yongzhi.wu@zatech.com>
2020-11-18 18:41:04 +0800

    集成OAuth2

---
+++ src/main/java/com/example/demo/config/OAuth2ServerConfig.java
@@ -0,0 +1,78 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+
+@Configuration
+public class OAuth2ServerConfig {
+
+    private static final String DEMO_RESOURCE_ID = "order";
+
+    @Configuration
+    @EnableResourceServer
+    protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+        @Override
+        public void configure(ResourceServerSecurityConfigurer resources) {
+            resources.resourceId(DEMO_RESOURCE_ID).stateless(true);
+        }
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            http
+                    // Since we want the protected resources to be accessible in the UI as well we need
+                    // session creation to be allowed (it's disabled by default in 2.0.6)
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                    .and()
+                    .requestMatchers().anyRequest()
+                    .and()
+                    .anonymous()
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/demo/api/*").authenticated();//配置order访问控制，必须认证过后才可以访问
+        }
+    }
+
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            String finalSecret = "{bcrypt}" + new BCryptPasswordEncoder().encode("123456");
+            //配置两个客户端,一个用于password认证一个用于client认证
+            clients.inMemory().withClient("client_1")
+                    .resourceIds(DEMO_RESOURCE_ID)
+                    .authorizedGrantTypes("client_credentials", "refresh_token")
+                    .scopes("select")
+                    .authorities("client")
+                    .secret(finalSecret)
+                    .and().withClient("client_2")
+                    .resourceIds(DEMO_RESOURCE_ID)
+                    .authorizedGrantTypes("password", "refresh_token")
+                    .scopes("select")
+                    .authorities("client")
+                    .secret("123456");
+        }
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+            //允许表单认证
+            oauthServer.allowFormAuthenticationForClients();
+        }
+
+    }
+
+}
--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +21,12 @@ public class DemoController {
         int count = 100;
         return "欢迎您" + userName + "，这是您的第 " + count + " 次登录";
 
+    }
+
+    @PostMapping("/api/auth")
+    public String getOrder(@RequestBody String args) {
+        //for debug
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "args : " + args;
     }
 }
